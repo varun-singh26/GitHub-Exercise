1dcedc249526c8b83e073ad9a1a06fadaeb8953c
varun-singh26 <149445092+varun-singh26@users.noreply.github.com>
2024-08-29 10:43:02 -0400

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by Varun Singh on 8/29/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
